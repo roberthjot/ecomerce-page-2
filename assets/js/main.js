526fd550a1c0524135261a9fd4d17555f8c69bca
gem installer <robertjot@gmail.com>
2021-07-13 01:00:44 -0400

    Este es el ultimo commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,5 +46,6 @@ function scrollActive(){
 /*===== CHANGE COLOR HEADER =====*/
 window.onscroll = ()=>{
     const nav = document.getElementById('header')
-    if(this.scrollY =>200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if(this.scrollY >=200) nav.classList.add('scroll-header'); 
+    else nav.classList.remove('scroll-header')
 }
